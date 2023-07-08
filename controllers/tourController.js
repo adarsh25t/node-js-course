c4a3a58ae2f1066bd4cc3b307fcced8498ab2958
Adarsh25t <adarsh25t@gmail.com>
2023-07-08 21:31:02 +0530

    pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,42 +22,53 @@ exports.createTour = async (req,res,next) => {
 }
 
 exports.getAllTours = async (req,res,next) => {
-    /*
-        
-    */
+    
     try {
 
-        // ? BUID QUERY
+        // * BUID QUERY
         const queryObj = {...req.query};
         const excludedFields = ['page','sort','limit','fields'];
         excludedFields.forEach(el=> delete queryObj[el]);
         
-        // ? ADVANCED FILTERING
+        // * ADVANCED FILTERING
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
         let query = Tour.find(JSON.parse(queryStr));
 
-        // ? SORTING
+        // * SORTING
         if (req.query.sort) {
 
-            // ? multiple items sort ( remove , and add empty space )
+            // * multiple items sort ( remove , and add empty space )
             let sortBy = req.query.sort.split(",").join(" ");
             query = query.sort(sortBy)
         }
 
-        // ? FIELDS ( SPECIFIC FIELDS )
+        // * FIELDS ( SPECIFIC FIELDS )
         if (req.query.fields) {
 
-            // ? multiple items  ( remove , and add empty space )
+            // * multiple items  ( remove , and add empty space )
             let fields = req.query.fields.split(",").join(" ");
             query = query.select(fields)
         }
+
+
+        // * PAGINATION 
+        if(req.query.page) {
+            const page = req.query.page * 1 || 1;
+            const limit = req.query.limit * 1 || 1;
+            const skip = (page - 1) * limit;
+            // page=2&limt=10 21-10 ( 2* 10 - 10 )
+            query = query.skip(skip).limit(limit);
+
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) throw new Error("This page is does not exist!")
+        }
         
-        // ? EXECUTE QUERY
+        // * EXECUTE QUERY
         const tours = await query;
         
 
-        // ? SEND RESPONSE
+        // * SEND RESPONSE
         res.status(200).json({
             status: "Success",
             results: tours.length,
@@ -74,13 +85,13 @@ exports.getAllTours = async (req,res,next) => {
     }
 }
 
+// * GET TOUR 
 exports.getTour = async (req,res,next) => {
 
     try {
-        
         let id = req.params.id;
-        
         const tour = await Tour.findById(id);
+
         res.status(200).json({
             status: "Success",
             data:{
@@ -96,6 +107,7 @@ exports.getTour = async (req,res,next) => {
     }
 }
 
+// * UPDATE TOUR
 exports.updateTour = async (req,res,next) => {
 
     try {
@@ -113,7 +125,6 @@ exports.updateTour = async (req,res,next) => {
             }
         })
         
-        
     } catch (error) {
         res.status(404).json({
             status: "fail",
@@ -122,11 +133,12 @@ exports.updateTour = async (req,res,next) => {
     }
 }
 
+// * DELETE TOUR 
 exports.deleteTour = async (req,res,next) => {
     
     try {
         let id = req.params.id;
-        const deleteTour = await Tour.findByIdAndDelete(id);
+        await Tour.findByIdAndDelete(id);
         res.status(204).json({
             status: "Success"
         })
