b304270c43509673113dcac0283e1a2c02021c1f
adarsh25t <adarsh25t@gmail.com>
2023-07-20 22:41:17 +0530

    error handler

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const tourRouter = require('./routes/tourRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
 let app = express();
 
@@ -8,5 +10,23 @@ app.use(express.json());
 
 
 app.use('/api/v1/tours',tourRouter);
+
+// ROUTER ERROR HANDLER 
+app.all('*', (req,res,next) => {
+    // res.status(404).json({
+    //     'status':'fail',
+    //     'message': `Can't find ${req.originalUrl} on the server!`
+    // })
+
+    // const err = new Error(`Can't find ${req.originalUrl} on the server!`);
+    // err.statusCode = 404;
+    // err.status = 'fail';
+    
+    next(new AppError(`Can't find ${req.originalUrl} on the server!`,404))
+})
+
+
+// ERROR HANDLING MIDLEWARE
+app.use(globalErrorHandler)
 
 module.exports = app;
